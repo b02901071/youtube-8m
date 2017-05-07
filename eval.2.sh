cdaeabff8061adfe7d490fb9da3add2d974ac21b
b02901071 <b02901071@ntu.edu.tw>
2017-05-07 19:44:26 +0800

    scripts, max_to_keep

--- eval.2.sh
+++ eval.2.sh
@@ -1,10 +1,11 @@
 CUDA_VISIBLE_DEVICES=1 \
 python train.py \
 --train_data_pattern='/share/corpus/yt8m/frame_level/train/train*.tfrecord' \
---model=GridLstmModel \
---train_dir=models/frame_level_grid_lstm_model_rgb_audio \
+--model=LstmModel \
+--train_dir=models/frame_level_lstm_model_rgb_audio \
 --frame_features=True \
 --feature_names="rgb, audio" \
 --feature_sizes="1024, 128" \
 --base_learning_rate=0.0002 \
---batch_size=256
+--batch_size=256 \
+--max_steps=120000 
--- eval.sh
+++ eval.sh
@@ -2,10 +2,11 @@ CUDA_VISIBLE_DEVICES=0 \
 python train.py \
 --train_data_pattern='/share/corpus/yt8m/frame_level/train/train*.tfrecord' \
 --model=GridLstmModel \
---train_dir=models/frame_level_grid_lstm_model_rgb_audio_5_layers \
+--train_dir=models/frame_level_grid_lstm_model_rgb_audio_8_layers \
 --frame_features=True \
 --feature_names="rgb, audio" \
 --feature_sizes="1024, 128" \
 --base_learning_rate=0.0002 \
 --batch_size=256 \
---lstm_layers=5
+--lstm_layers=8 \
+--max_steps=120000
